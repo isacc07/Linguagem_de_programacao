ed663f13c93d2a2e5b1e1f17896c2135f50e1426
isacc07 <isacbjj07@gmail.com>
2026-06-02 16:47:30 -0300

    Add files via upload

---
+++ PrimeiroProjetoJava/src/inicial/App.java
@@ -0,0 +1,22 @@
+package inicial;
+
+public class App {
+
+	public static void main(String[] args) {
+		String nome = "Isac";
+		
+		System.out.println("Olá, Mundo!!");
+		System.out.print("Seja bem vindo "+ nome +", ");
+		System.out.print("Essa é sua primeira aula de Java!!");
+		
+		int idade = 17;
+		
+		
+		System.out.printf("Meu nome é %s e tenho %d anos.\n\n", nome, idade);
+		
+		double pi = 3.141592926536;
+		
+		System.out.printf ("O valor de pi é %.2f", pi);
+	}
+
+}
